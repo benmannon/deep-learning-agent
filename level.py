f012f430a32a0d59e4bc411c835499ee2709e06e
benmannon <benmannon@gmail.com>
2016-12-24 18:34:06 -0500

    Added Level and related classes

---
+++ level.py
@@ -0,0 +1,63 @@
+from math import pi
+
+
+def level_square():
+    grid = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+
+    coins = [
+        [2.0, 7.0],
+        [2.0, 8.0],
+        [3.0, 8.0],
+        [4.0, 4.0],
+        [4.0, 6.0],
+        [6.0, 4.0],
+        [6.0, 6.0],
+        [7.0, 2.0],
+        [7.0, 8.0],
+        [8.0, 2.0],
+        [8.0, 3.0],
+        [8.0, 7.0],
+        [8.0, 8.0]
+    ]
+
+    agent = Agent(2.0, 2.0, pi / 4.0)
+
+    return Level(grid, coins, agent)
+
+
+class Level:
+    agent = None
+    coins = None
+
+    def __init__(self, grid=[], coins=[], agent=Agent()):
+        self.grid = grid
+        self.coins_origin = coins
+        self.agent_origin = agent
+        self.reset()
+
+    def reset(self):
+        self.coins = self.coins_origin
+        self.agent = self.agent_origin
+
+
+class Agent:
+    def __init__(self, x=0.0, y=0.0, theta=0.0):
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+
+class Coin:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
