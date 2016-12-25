1c220bad4e89ae891b071d8c9778a20c888405a7
benmannon <benmannon@gmail.com>
2016-12-24 20:41:03 -0500

    Added missing semicolons to fragment shader

--- draw.py
+++ draw.py
@@ -17,11 +17,11 @@ def _grid_program():
     """
 
     grid_fragment = """
-        uniform sampler2D texture
+        uniform sampler2D texture;
         varying vec2 v_texcoord;
         void main()
         {
-            gl_FragColor = texture2D(texture, v_texcoord)
+            gl_FragColor = texture2D(texture, v_texcoord);
         }
     """
 
