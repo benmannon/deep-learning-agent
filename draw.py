7d1f5d4ec84d9d2c8f9d1fac0f3ee4fbe4d8ce07
benmannon <benmannon@gmail.com>
2016-12-25 13:26:56 -0500

    Implemented agent drawing

--- draw.py
+++ draw.py
@@ -9,6 +9,7 @@ class Draw:
         self._lock = Lock()
         self._grid = self._grid_program(grid_shape)
         self._coin = self._coin_program()
+        self._agent = self._agent_program()
 
     def update(self, level):
         self._lock.acquire()
@@ -17,6 +18,9 @@ class Draw:
             self._grid['texture'] = np.reshape(np.repeat(grid, 3), grid.shape + (3,))
             self._coin['texcoord'] = [(-1, -1), (-1, +1), (+1, +1), (+1, -1)] * len(level.coins)
             self._coin['position'] = self.coin_positions(level.coins)
+            self._agent['texcoord'] = [(-1, -1), (-1, +1), (+1, +1), (+1, -1)]
+            self._agent['position'] = self.agent_position(level.agent)
+            self._agent['theta'] = level.agent.theta
         finally:
             self._lock.release()
 
@@ -33,6 +37,16 @@ class Draw:
             offset += 4
         return positions;
 
+    @staticmethod
+    def agent_position(agent):
+        scaled = [agent.x / 5 - 1, agent.y / 5 - 1]
+        position = [None] * 4
+        position[0] = (scaled[0] - 0.09, scaled[1] - 0.09)
+        position[1] = (scaled[0] - 0.09, scaled[1] + 0.09)
+        position[2] = (scaled[0] + 0.09, scaled[1] + 0.09)
+        position[3] = (scaled[0] + 0.09, scaled[1] - 0.09)
+        return position;
+
     def run(self):
 
         window = app.Window(width=1024, height=1024, aspect=1)
@@ -44,6 +58,7 @@ class Draw:
                 window.clear()
                 self._grid.draw(gl.GL_TRIANGLE_STRIP)
                 self._coin.draw(gl.GL_QUADS)
+                self._agent.draw(gl.GL_QUADS)
             finally:
                 self._lock.release()
 
@@ -106,3 +121,46 @@ class Draw:
         """
 
         return gloo.Program(vertex, fragment)
+
+    @staticmethod
+    def _agent_program():
+        vertex = """
+            attribute vec2 position;
+            attribute vec2 texcoord;
+            varying vec2 v_texcoord;
+            void main()
+            {
+                gl_Position = vec4(position, 0.0, 1.0);
+                v_texcoord = texcoord;
+            }
+        """
+
+        fragment = """
+            #define M_PI 3.141592653589793
+            uniform vec4 circle_color = vec4(0.31, 0.89, 0.706, 1.0);
+            uniform vec4 pointer_color = vec4(0.83, 0.98, 0.93, 1.0);
+            uniform vec4 bkg_color = vec4(0.31, 0.89, 0.706, 0.0);
+            uniform float threshold = M_PI / 4;
+            uniform float theta;
+            varying vec2 v_texcoord;
+            void main()
+            {
+                float dist = sqrt(dot(v_texcoord, v_texcoord));
+                if (dist < 1)
+                {
+                    vec2 coord_unit = v_texcoord / dist;
+                    float theta_actual = atan(coord_unit.y, coord_unit.x);
+                    float theta_diff = abs(theta - theta_actual);
+                    if (theta_diff > threshold)
+                        gl_FragColor = circle_color;
+                    else
+                        gl_FragColor = pointer_color;
+                }
+                else
+                {
+                    gl_FragColor = bkg_color;
+                }
+            }
+        """
+
+        return gloo.Program(vertex, fragment, count=4)
