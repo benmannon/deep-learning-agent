1dbfad601277328cfb228032f1796f4dd9a88ea2
benmannon <benmannon@gmail.com>
2017-01-04 19:07:01 -0500

    Changed ShallowAgent to select an action based on probabilities, instead of just choosing the maximum option.  The output is effectively a policy gradient.

--- agent.py
+++ agent.py
@@ -31,21 +31,38 @@ class ShallowAgent(Agent):
     def __init__(self, input_shape, output_range):
         super(ShallowAgent, self).__init__(input_shape, output_range)
         self._input_size = np.prod(self._input_shape)
-        self._sess, self._x, self._q_max = self.build_model()
+        self._sess, self._x, self._p = self.build_model()
 
     def build_model(self):
         x = tf.placeholder(tf.float32, [None, self._input_size])
         w = tf.Variable(weights([self._input_size, self._output_range]))
         b = tf.Variable(biases([self._output_range]))
-        q = tf.nn.softmax(tf.matmul(x, w) + b)
-        q_max = tf.argmax(q, 1)
+        p = tf.nn.softmax(tf.matmul(x, w) + b)
 
         init = tf.global_variables_initializer()
         sess = tf.Session()
         sess.run(init)
 
-        return sess, x, q_max
+        return sess, x, p
 
     def eval(self, agent_input):
         inputs = np.reshape(agent_input, (1, self._input_size))
-        return self._sess.run(self._q_max, feed_dict={self._x: inputs})
+        p = self._sess.run(self._p, feed_dict={self._x: inputs})
+        return self._select(np.reshape(p, (-1,)))
+
+    @staticmethod
+    def _select(p):
+
+        # randomly select an index over an array of normalized probabilities
+        r = random.random()
+        odds = 0.0
+        i = 0
+        for prob in p:
+            odds += prob
+            if r <= odds:
+                return i
+            i += 1
+
+        # should never get this far, but return the last item just in case
+        print 'warning: total odds, %s > 1.0' % odds
+        return i - 1
