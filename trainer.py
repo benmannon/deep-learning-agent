88cfe79fdb7ae998e94b2f8ba58fc3e79c4b0de1
benmannon <benmannon@gmail.com>
2017-01-05 18:21:16 -0500

    Renamed constant

--- trainer.py
+++ trainer.py
@@ -31,7 +31,7 @@ _AGENT_POINTER_BRIGHTNESS = 0.5
 _BKG_COLOR = [0.0, 0.3, 0.5]
 
 # learning parameters
-_XP_BUFFER_SIZE = 10000
+_REPLAY_BUFFER_SIZE = 10000
 _REPLAY_BATCH_SIZE = 1000
 _REWARD_DISCOUNT_FACTOR = 0.95
 
@@ -51,7 +51,7 @@ class Trainer:
 
     def train(self, sim, first_input):
 
-        learner = Learner(_XP_BUFFER_SIZE, _REPLAY_BATCH_SIZE, _REWARD_DISCOUNT_FACTOR)
+        learner = Learner(_REPLAY_BUFFER_SIZE, _REPLAY_BATCH_SIZE, _REWARD_DISCOUNT_FACTOR)
 
         agent_input = first_input
         reward = 0.0
