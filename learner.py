5d888c4de25d88b4b87286f165a0374e334763c5
benmannon <benmannon@gmail.com>
2017-01-11 21:30:38 -0500

    Stored experiences as parallel lists of states, actions, and rewards instead of a list of tuples

--- learner.py
+++ learner.py
@@ -4,7 +4,9 @@ from xp_buffer import XpBuffer
 
 class Learner:
     def __init__(self, buffer_size, batch_size, discount_factor, n_inputs, n_channels, n_actions):
-        self._episode = []
+        self._ep_states = []
+        self._ep_actions = []
+        self._ep_rewards = []
         self._xp_buf = XpBuffer(buffer_size)
         self._batch_size = batch_size
         self._discount_factor = discount_factor
@@ -12,43 +14,35 @@ class Learner:
         self._recent_state = None
         self._recent_action = None
 
-    def _add_xp(self, xp):
-        self._episode += [xp]
+    def _add_xp(self, state, action, reward):
+        self._ep_states.append(state)
+        self._ep_actions.append(action)
+        self._ep_rewards.append(reward)
 
     def _end_episode(self):
-        self._xp_buf.append(self._discount(self._episode))
-        self._episode = []
+        self._xp_buf.append(self._ep_states, self._ep_actions, self._discount(self._ep_rewards))
+        self._ep_states = []
+        self._ep_actions = []
+        self._ep_rewards = []
 
-    def _discount(self, xps):
+    def _discount(self, rewards):
         gamma = self._discount_factor
         total_reward = 0.0
-        d_xps = []
-        for xp in reversed(xps):
-            total_reward = gamma * total_reward + xp[2]
-            d_xps.append((xp[0], xp[1], total_reward))
-        return reversed(d_xps)
+        d_rewards = []
+        for reward in reversed(rewards):
+            total_reward = gamma * total_reward + reward
+            d_rewards.append(total_reward)
+        return reversed(d_rewards)
 
     def _learn(self):
         if self._xp_buf.size > 0:
-            xp_samples = self._xp_buf.samples(self._batch_size)
-            states, actions, rewards = self._split(xp_samples)
+            states, actions, rewards = self._xp_buf.samples(self._batch_size)
             self._agent.train(states, actions, rewards)
-
-    @staticmethod
-    def _split(xps):
-        states = []
-        actions = []
-        rewards = []
-        for xp in xps:
-            states.append(xp[0])
-            actions.append(xp[1])
-            rewards.append(xp[2])
-        return states, actions, rewards
 
     def perceive(self, state, reward, terminal):
 
         if self._recent_state:
-            self._add_xp((self._recent_state, self._recent_action, reward))
+            self._add_xp(self._recent_state, self._recent_action, reward)
 
         action = self._agent.eval_e_greedy(state)
 
--- xp_buffer.py
+++ xp_buffer.py
@@ -4,21 +4,36 @@ import random
 class XpBuffer:
     def __init__(self, capacity):
         self._capacity = capacity
-        self._buf = []
+        self._s = []
+        self._a = []
+        self._r = []
 
-    def append(self, xps):
-        self._buf += xps
-        overfill = len(self._buf) - self._capacity
+    def append(self, states, actions, rewards):
+        self._s += states
+        self._a += actions
+        self._r += rewards
+        overfill = len(self._s) - self._capacity
         if overfill > 0:
-            del self._buf[:overfill]
+            del self._s[:overfill]
+            del self._a[:overfill]
+            del self._r[:overfill]
 
     def samples(self, n):
-        elements = []
+
+        # states, actions, rewards
+        s = []
+        a = []
+        r = []
+
+        # sample randomly
         for _ in range(0, n):
-            element = self._buf[random.randrange(0, len(self._buf))]
-            elements.append(element)
-        return elements
+            i = random.randrange(0, len(self._s))
+            s += [self._s[i]]
+            a += [self._a[i]]
+            r += [self._r[i]]
+
+        return s, a, r
 
     @property
     def size(self):
-        return len(self._buf)
+        return len(self._s)
