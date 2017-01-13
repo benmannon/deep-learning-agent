9941404682f39400b1943ee0ee84adbf3557df09
benmannon <benmannon@gmail.com>
2017-01-12 21:33:53 -0500

    Implemented optional thompson sampling exploration strategy using dropout

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 _OP_INPUTS = 'inputs'
+_OP_DROPOUT = 'dropout'
 _OP_P = 'p'
 _OP_GREEDY = 'greedy'
 _OP_E_GREEDY = 'e_greedy'
@@ -9,6 +10,8 @@ _OP_REWARDS = 'rewards'
 _OP_ACTIONS = 'actions'
 _OP_TRAIN = 'train'
 
+_DROPOUT_OFF = 0.0
+_DROPOUT_ON = 1.0
 
 class Agent(object):
     def __init__(self, n_inputs, n_channels, n_outputs):
@@ -17,7 +20,8 @@ class Agent(object):
     def build_model(self, n_inputs, n_channels, n_outputs):
         # feed forward
         x = tf.placeholder(tf.float32, [None, n_inputs, n_channels])
-        q = self._model_q(x, n_inputs, n_channels, n_outputs)
+        dropout = tf.placeholder(tf.float32, [])
+        q = self._model_q(x, dropout, n_inputs, n_channels, n_outputs)
 
         p = tf.nn.softmax(q)
         greedy = tf.argmax(q, 1)
@@ -35,6 +39,7 @@ class Agent(object):
 
         ops = {
             _OP_INPUTS: x,
+            _OP_DROPOUT: dropout,
             _OP_P: p,
             _OP_GREEDY: greedy,
             _OP_E_GREEDY: e_greedy,
@@ -70,25 +75,35 @@ class Agent(object):
 
     def eval_pg(self, state):
         return self._sess.run(self._ops[_OP_P], feed_dict={
-            self._ops[_OP_INPUTS]: [state]
+            self._ops[_OP_INPUTS]: [state],
+            self._ops[_OP_DROPOUT]: _DROPOUT_OFF,
         })[0]
 
     def eval_greedy(self, state):
         return self._sess.run(self._ops[_OP_GREEDY], feed_dict={
-            self._ops[_OP_INPUTS]: [state]
+            self._ops[_OP_INPUTS]: [state],
+            self._ops[_OP_DROPOUT]: _DROPOUT_OFF,
         })[0]
 
     def eval_e_greedy(self, state, epsilon):
         return self._sess.run(self._ops[_OP_E_GREEDY], feed_dict={
             self._ops[_OP_INPUTS]: [state],
+            self._ops[_OP_DROPOUT]: _DROPOUT_OFF,
             self._ops[_OP_EPSILON]: epsilon
+        })[0]
+
+    def eval_thompson_sample(self, state):
+        return self._sess.run(self._ops[_OP_GREEDY], feed_dict={
+            self._ops[_OP_INPUTS]: [state],
+            self._ops[_OP_DROPOUT]: _DROPOUT_ON,
         })[0]
 
     def train(self, states, actions, rewards):
         self._sess.run(self._ops[_OP_TRAIN], feed_dict={
             self._ops[_OP_INPUTS]: states,
             self._ops[_OP_ACTIONS]: actions,
-            self._ops[_OP_REWARDS]: rewards
+            self._ops[_OP_REWARDS]: rewards,
+            self._ops[_OP_DROPOUT]: _DROPOUT_OFF,
         })
 
 
@@ -101,12 +116,15 @@ class RandomAgent(Agent):
 
 
 class LinearAgent(Agent):
-    def _model_q(self, x, n_inputs, n_channels, n_outputs):
+    def _model_q(self, x, dropout, n_inputs, n_channels, n_outputs):
         x_size = n_inputs * n_channels
         x_flat = tf.reshape(x, [-1, x_size])
 
+        keep_prob = 1 - dropout * 0.5
+        drop_x = tf.nn.dropout(x_flat, keep_prob)
+
         q = tf.contrib.layers.fully_connected(
-            inputs=x_flat,
+            inputs=drop_x,
             num_outputs=n_outputs,
             activation_fn=None,
             weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
@@ -117,12 +135,15 @@ class LinearAgent(Agent):
 
 
 class ReluAgent(Agent):
-    def _model_q(self, x, n_inputs, n_channels, n_outputs):
+    def _model_q(self, x, dropout, n_inputs, n_channels, n_outputs):
         x_size = n_inputs * n_channels
         x_flat = tf.reshape(x, [-1, x_size])
 
+        keep_prob = 1 - dropout * 0.5
+        drop_x = tf.nn.dropout(x_flat, keep_prob)
+
         q = tf.contrib.layers.fully_connected(
-            inputs=x_flat,
+            inputs=drop_x,
             num_outputs=n_outputs,
             activation_fn=tf.nn.relu,
             weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
@@ -133,12 +154,15 @@ class ReluAgent(Agent):
 
 
 class SigmoidAgent(Agent):
-    def _model_q(self, x, n_inputs, n_channels, n_outputs):
+    def _model_q(self, x, dropout, n_inputs, n_channels, n_outputs):
         x_size = n_inputs * n_channels
         x_flat = tf.reshape(x, [-1, x_size])
 
+        keep_prob = 1 - dropout * 0.5
+        drop_x = tf.nn.dropout(x_flat, keep_prob)
+
         q = tf.contrib.layers.fully_connected(
-            inputs=x_flat,
+            inputs=drop_x,
             num_outputs=n_outputs,
             activation_fn=tf.sigmoid,
             weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
@@ -149,12 +173,15 @@ class SigmoidAgent(Agent):
 
 
 class TanhAgent(Agent):
-    def _model_q(self, x, n_inputs, n_channels, n_outputs):
+    def _model_q(self, x, dropout, n_inputs, n_channels, n_outputs):
         x_size = n_inputs * n_channels
         x_flat = tf.reshape(x, [-1, x_size])
 
+        keep_prob = 1 - dropout * 0.5
+        drop_x = tf.nn.dropout(x_flat, keep_prob)
+
         q = tf.contrib.layers.fully_connected(
-            inputs=x_flat,
+            inputs=drop_x,
             num_outputs=n_outputs,
             activation_fn=tf.tanh,
             weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
