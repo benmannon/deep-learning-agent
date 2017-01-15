8362959083e29c4afdfbf1eb91f5029cf390352b
benmannon <benmannon@gmail.com>
2017-01-15 12:55:07 -0500

    Changed channel values from lists to tuples

--- vision.py
+++ vision.py
@@ -5,9 +5,9 @@ import numpy as np
 
 CHANNEL_NUM = 2
 
-_CHANNELS_NONE = [0, 0]
-_CHANNELS_WALL = [1, 0]
-_CHANNELS_COIN = [0, 1]
+_CHANNELS_NONE = (0, 0)
+_CHANNELS_WALL = (1, 0)
+_CHANNELS_COIN = (0, 1)
 
 
 def _fog(channels, distance, attenuation):
