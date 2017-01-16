111f89fb711597be84ca6af2c24fc47547bf9b20
benmannon <benmannon@gmail.com>
2017-01-15 20:02:50 -0500

    Converted methods to functions

--- simulator.py
+++ simulator.py
@@ -11,17 +11,43 @@ from vision import Vision
 CHANNEL_NUM = vision.CHANNEL_NUM
 
 
+def _color(channels, args):
+    if channels[0] > 0:
+        return args.grid_color + [channels[0]]
+    elif channels[1] > 0:
+        return args.coin_color + [channels[1]]
+    else:
+        return [0.0, 0.0, 0.0, 0.0]
+
+
+def _lines(signals, args):
+    signal_lines = []
+    for signal in signals:
+        a = [signal.ax, signal.ay]
+        b = [signal.bx, signal.by]
+        signal_lines.append(Line(a, b, _color(signal.channels, args)))
+    return signal_lines
+
+
+def _sight_colors(signals, args):
+    colors = []
+    for signal in signals:
+        colors.append(_color(signal.channels, args))
+    return colors
+
+
+def _channels(signals):
+    channels = []
+    for signal in signals:
+        channels.append(signal.channels)
+    return channels
+
+
 class Simulator:
     def __init__(self, args):
 
-        self._reward_coin = args.reward_coin
-        self._reward_win = args.reward_win
-        self._reward_loss = args.reward_loss
-        self._reward_collision = args.reward_collision
-
+        self._args = args
         self._lvl = level.square()
-        self._grid_color = args.grid_color
-        self._coin_color = args.coin_color
 
         self._draw = Draw(args, self._lvl.grid.shape)
         self._ctrl = Controller(args, self._lvl)
@@ -33,35 +59,13 @@ class Simulator:
 
         # prepare first frame
         sightline = self._vision.look()
-        self._draw.update(self._lvl, self._lines(sightline), self._sight_colors(sightline))
+        self._draw.update(self._lvl, _lines(sightline, self._args), _sight_colors(sightline, self._args))
 
         if after_init is not None:
-            sight_channels = self._channels(sightline)
+            sight_channels = _channels(sightline)
             threading.Thread(target=after_init, args=[self, sight_channels]).start()
 
         self._draw.run(key_handler=on_key_press, close_handler=on_close)
-
-    def _color(self, channels):
-        if channels[0] > 0:
-            return self._grid_color + [channels[0]]
-        elif channels[1] > 0:
-            return self._coin_color + [channels[1]]
-        else:
-            return [0.0, 0.0, 0.0, 0.0]
-
-    def _lines(self, signals):
-        signal_lines = []
-        for signal in signals:
-            a = [signal.ax, signal.ay]
-            b = [signal.bx, signal.by]
-            signal_lines.append(Line(a, b, self._color(signal.channels)))
-        return signal_lines
-
-    def _sight_colors(self, signals):
-        colors = []
-        for signal in signals:
-            colors.append(self._color(signal.channels))
-        return colors
 
     def step(self, action):
 
@@ -72,31 +76,24 @@ class Simulator:
         is_colliding = self._ctrl.step(action)
         coins_left = len(self._lvl.coins)
         coins_collected = coins_available - coins_left
-        reward = float(coins_collected) * self._reward_coin
+        reward = float(coins_collected) * self._args.reward_coin
         end = False
 
         if is_colliding:
-            reward += self._reward_collision
+            reward += self._args.reward_collision
 
         # no more coins? out of time? reset the level
         if coins_left == 0:
-            reward += self._reward_win
+            reward += self._args.reward_win
             self._time_step = self._lvl.time
             self._lvl.reset()
             end = True
         elif self._time_step <= 0:
-            reward += self._reward_loss
+            reward += self._args.reward_loss
             self._time_step = self._lvl.time
             self._lvl.reset()
             end = True
 
         sightline = self._vision.look()
-        self._draw.update(self._lvl, self._lines(sightline), self._sight_colors(sightline))
-        return self._channels(sightline), reward, end
-
-    @staticmethod
-    def _channels(signals):
-        channels = []
-        for signal in signals:
-            channels.append(signal.channels)
-        return channels
+        self._draw.update(self._lvl, _lines(sightline, self._args), _sight_colors(sightline, self._args))
+        return _channels(sightline), reward, end
